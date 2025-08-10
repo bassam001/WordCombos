3ddb0da6517ba70720b75771bc276a93b85c88e9
Pc <Pc@LPC>
2025-08-10 21:20:17 +0200

    add solution and adapterProject

---
+++ WordCombos.Adapters/FileWordRepository.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+using WordCombos.Core.Interfaces;
+
+namespace WordCombos.Adapters;
+
+public sealed class FileWordRepository : IWordRepository
+{
+    private readonly string _path ;
+    private readonly bool _caseInsensitive;
+
+    public FileWordRepository(string path, bool caseInsensitive)
+    {
+        _path = path;
+        _caseInsensitive = caseInsensitive;
+    }
+
+    public ISet<string> GetAllWords()
+    {
+        if (!File.Exists(_path))
+            throw new FileNotFoundException($"Input file not found at: {_path}\nBase: {AppContext.BaseDirectory}");
+
+        var set = new HashSet<string>(_caseInsensitive ? StringComparer.OrdinalIgnoreCase : StringComparer.Ordinal);
+
+        // Only UTF8 files !!
+        foreach (var line in File.ReadLines(_path, Encoding.UTF8))
+        {
+            var w = line.Trim();
+            if (w.Length == 0) continue;
+            set.Add(_caseInsensitive ? w.ToLowerInvariant() : w);
+        }
+
+        return set;
+    }
+}
